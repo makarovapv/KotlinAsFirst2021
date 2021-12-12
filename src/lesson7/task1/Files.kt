67a24edda0431c8fac93bc286b49974b39925cf7
makarovapv <makrolina@bk.ru>
2021-12-12 22:35:17 +0300

    les 7

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -335,7 +335,7 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
         }
         val textLines = text.split("__NEWLINE__")
         var p = false
-        if (text.isBlank()) it.write("<p></p>")
+        if (text.replace("__NEWLINE__", "\n").isBlank()) it.write("<p></p>")
         textLines.forEachIndexed { index, text ->
             if (index != 0) {
                 if (textLines[index - 1].isBlank() && p) {
